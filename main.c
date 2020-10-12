f107f5e2967dddf574d3fb4db1a7e2dce3d7178b
nomadae <gilberto.carlos@comunidad.unam.mx>
2020-10-12 12:13:16 -0500

    new assignments

--- main.c
+++ main.c
@@ -2,15 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
 
-// Tarea: crear biblioteca que reciba un argumento, lo copie a una cadena y lo imprima
-// que imprima el número de bytes
-// que imprima el codigo ascii en forma decimal x0000001 --> 1
+TAREA 1: 
+crear biblioteca que reciba un argumento, 
+lo copie a una cadena y lo imprima
+que imprima el número de bytes
+que imprima el codigo ascii en forma decimal 
+
+TAREA 2: 
+como hace un cast de char a antero
+apuntador de enteros 
+direccion de memoria
+valor en la direccion de memoria
+
+*/
 
 int main(int argn, char **argv){
 
+    char* display;
+    display = malloc(sizeof(argv[1]));
     
-    char* display = malloc(sizeof(argv[1]));
     display = command_line_parse(argn, argv);
     printf("%s\n", display);
     get_bsize(display);
@@ -18,3 +30,4 @@ int main(int argn, char **argv){
     free(display);
     return 1;
 }
+
